b244b6e32125eff5cb8758e9c7a1ed135fce0688
hard <harleydav@mail.ru>
2021-02-08 15:01:59 +0300

    fragments

--- tasks/fragments/src/main/java/com/hard/application/MainActivity.java
+++ tasks/fragments/src/main/java/com/hard/application/MainActivity.java
@@ -4,10 +4,18 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hard.application.fragments.Fragment1;
+
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Fragment1 fragment1 = Fragment1.newInstance("", "");
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, fragment1, null)
+                .commit();
     }
 }

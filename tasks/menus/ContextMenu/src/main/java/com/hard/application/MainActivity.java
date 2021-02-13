ce8d0095a386595f46a45b85242e0d5110d2e057
hard <harleydav@mail.ru>
2021-02-13 20:48:55 +0300

    PopupMenu

--- tasks/menus/ContextMenu/src/main/java/com/hard/application/MainActivity.java
+++ tasks/menus/ContextMenu/src/main/java/com/hard/application/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        View view = findViewById(R.id.textView);
+        View view = findViewById(R.id.view);
 
         // long click
         registerForContextMenu(view);

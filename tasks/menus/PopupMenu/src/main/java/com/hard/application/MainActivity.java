a32188513da69a9b5e3466c3ee56b8f3a12aceb7
hard <harleydav@mail.ru>
2021-02-13 20:44:49 +0300

    PopupMenu

---
+++ tasks/menus/PopupMenu/src/main/java/com/hard/application/MainActivity.java
@@ -0,0 +1,47 @@
+package com.hard.application;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        // xml
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+
+        // programmatically
+        menu.add(1, R.id.item_2, 1, "item 2");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.item_1:
+                // TODO
+                return true;
+            case R.id.item_2:
+                // TODO
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
